0a8eede0845e2783ed7e3d90f3a5af882ec5f1aa
tuan <tuanbosstrum@gmail.com>
2022-05-12 08:42:49 +0700

    bruhhhhh

--- src/components/Search.js
+++ src/components/Search.js
@@ -29,6 +29,7 @@ class Search extends Component {
       <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
         <div className="input-group">
           <input
+            name="keyword"
             type="text"
             className="form-control"
             placeholder="Nhập từ khóa..."
